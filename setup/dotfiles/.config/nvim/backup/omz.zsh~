89e242c7de130d1d857861ca5237c433bc0b3943
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-12-09 20:19:27 +0200

    feat

---
+++ setup/dotfiles/.config/nvim/backup/omz.zsh~
@@ -0,0 +1,18 @@
+# ------------------------------
+# omz config 
+# ------------------------------
+
+# Path to your oh-my-zsh installation.
+export ZSH="$HOME/.oh-my-zsh"
+
+source $ZSH/oh-my-zsh.sh
+
+# Remind about update when it is time
+zstyle ':omz:update' mode reminder  
+
+plugins=(git docker zsh-syntax-highlighting zsh-autosuggestions you-should-use)
+
+
+# ------------------------------
+# end omz config 
+# ------------------------------
---
+++ setup/dotfiles/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,40 @@
+-- ~/nvim/lua/slydragonn/plugins/mason.lua
+
+return {
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		require("mason").setup()
+
+		require("mason-lspconfig").setup({
+			automatic_installation = true,
+			ensure_installed = {
+				"cssls",
+				"eslint",
+				"html",
+				"jsonls",
+				"pyright",
+				"tailwindcss",
+				"intelephense",
+				"gopls",
+				"jdtls",
+				"sqlls",
+			},
+		})
+
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				"prettier",
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"pylint",
+				"eslint_d",
+				"php-cs-fixer", -- PHP formatter
+			},
+		})
+	end,
+}
