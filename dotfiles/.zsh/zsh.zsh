2307e5bd856d2a2d6d7e4d6c6eb2ef0337e749ef
Bohdan Buchok <bohdanbuchok@gmail.com>
2024-12-09 10:57:50 +0200

    feat

--- dotfiles/.zsh/zsh.zsh
+++ dotfiles/.zsh/zsh.zsh
@@ -2,20 +2,22 @@
 # zsh config 
 # ------------------------------
 
-autoload -Uz vcs_info
+autoload -Uz vcs_info compinit
+
+compinit
+
 precmd() {
     vcs_info
 }
-zstyle ':vcs_info:git:*' formats '[%b]'
-setopt prompt_subst
 
-ZSH_THEME="strug"
+zstyle ':vcs_info:git:*' formats '[%b]'
+
+setopt prompt_subst
 
 # Prompt config
 PROMPT='%F{92} ╭─%f%B%F{220}%~%f %F{92}${vcs_info_msg_0_}%f%b
 %F{92} ╰$ %f'
 RPROMPT='%B%F{92}%f%b'
-
 
 # ------------------------------
 # end zsh config 
