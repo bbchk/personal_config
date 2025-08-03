83fa98a7ac0b0dc3071329c126e122403456379c
Bohdan Buchok <bohdanbuchok@gmail.com>
2025-08-03 18:42:59 +0300

    new omz

--- setup/k8s/setup.sh
+++ setup/k8s/setup.sh
@@ -6,5 +6,16 @@ apt update
 apt install sudo vim
 sudo update-alternatives --config editor
 
+# visudo thing
+
 sudo systemctl mask  "dev-*.swap"
 
+
+
+# HandlePowerKey=poweroff        # Can be poweroff, reboot, halt, ignore
+# HandleLidSwitch=suspend        # Can be suspend, ignore, lock, etc.
+# HandleLidSwitchDocked=ignore   # Controls lid action when docked
+# IdleAction=ignore              # Can be suspend, poweroff, etc.
+# IdleActionSec=30min            # Time before IdleAction triggers
+
+sudo systemctl restart systemd-logind
