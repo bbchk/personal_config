d2e3a1ba76a9268391dc36e4d960833f0d94b000
Bohdan Buchok <bohdanbuchok@gmail.com>
2025-08-06 10:21:42 +0300

    kubectl krew, kubectx, kubens install

--- dotfiles/.zsh/user.zsh
+++ dotfiles/.zsh/user.zsh
@@ -8,12 +8,15 @@ export PATH="$HOME/.symfony5/bin:$PATH"
 export JAVA_HOME="/usr/lib/jvm/java-17-openjdk-amd64"
 export PATH=$JAVA_HOME/bin:$PATH
 
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
 export PATH="$PATH:/usr/lib/docker/cli-plugins"
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
 export GPG_TTY=$(tty)
 
 export MANPATH="/usr/local/man:$MANPATH"
+
 
 # Manually set language environment
 export LANG=en_US.UTF-8
@@ -47,6 +50,7 @@ fi
 
 source <(fzf --zsh)
 source <(kubectl completion zsh)
+
 
 export PATH="$HOME/pers/scripts:$PATH"
 
--- setup/fedora/kubectl.sh
+++ setup/fedora/kubectl.sh
@@ -7,5 +7,15 @@ echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
 
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
+
 kubectl krew install ns
 kubectl krew install ctx
