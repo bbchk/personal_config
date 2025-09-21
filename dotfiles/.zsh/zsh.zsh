a72fefc930089efa280c509838b0288d889e42b6
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2025-09-21 11:02:43 +0300

    coloring

--- dotfiles/.zsh/zsh.zsh
+++ dotfiles/.zsh/zsh.zsh
@@ -22,6 +22,8 @@ PROMPT='%F{155} ╭─%f%B%F{227}%~%f %F{155}${vcs_info_msg_0_}%f%b
 %F{155} ╰$ %f'
 RPROMPT='%B%F{155}%f%b'
 
+export FZF_DEFAULT_OPTS="--color=fg:white,bg:black,hl:red,fg+:white,bg+:black,hl+:227,prompt:155,pointer:227"
+
 # ------------------------------
 # end zsh config 
 # ------------------------------
