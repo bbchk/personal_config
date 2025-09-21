253181f705adc33e4d594485a4b6f32c02cab0bf
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2025-09-21 10:44:59 +0300

    user-dirs

--- dotfiles/.zsh/system.zsh
+++ dotfiles/.zsh/system.zsh
@@ -5,14 +5,15 @@
 export PATH="/usr/bin:$PATH"
 export LD_LIBRARY_PATH="/usr/lib:$LD_LIBRARY_PATH"
 
-export XDG_DESKTOP_DIR="$HOME/pers/xdg/Desktop"
-export XDG_DOWNLOAD_DIR="$HOME/downloads"
-export XDG_TEMPLATES_DIR="$HOME/pers/xdg/Templates"
-export XDG_PUBLICSHARE_DIR="$HOME/pers/xdg/Public"
-export XDG_DOCUMENTS_DIR="$HOME/pers/xdg/Documents"
-export XDG_MUSIC_DIR="$HOME/pers/xdg/Music"
-export XDG_PICTURES_DIR="$HOME/pers/xdg/Pictures"
-export XDG_VIDEOS_DIR="$HOME/pers/xdg/Videos"
+# it should be done in ~/.config/user-dirs.dirs
+# export XDG_DESKTOP_DIR="$HOME/pers/xdg/Desktop"
+# export XDG_DOWNLOAD_DIR="$HOME/downloads"
+# export XDG_TEMPLATES_DIR="$HOME/pers/xdg/Templates"
+# export XDG_PUBLICSHARE_DIR="$HOME/pers/xdg/Public"
+# export XDG_DOCUMENTS_DIR="$HOME/pers/xdg/Documents"
+# export XDG_MUSIC_DIR="$HOME/pers/xdg/Music"
+# export XDG_PICTURES_DIR="$HOME/pers/xdg/Pictures"
+# export XDG_VIDEOS_DIR="$HOME/pers/xdg/Videos"
 
 # if [ -z "$SSH_AUTH_SOCK" ]; then
 #   eval `ssh-agent -s` > /dev/null;
--- setup/common/filesystem.sh
+++ setup/common/filesystem.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 # ====================================
 
-mkdir -p ~/dev
+mkdir -p "$HOME/dev"
 
 mv "$HOME/Desktop" "$HOME/pers/xdg/Desktop"
 mv "$HOME/Documents" "$HOME/pers/xdg/Documents"
@@ -13,6 +13,6 @@ mv "$HOME/Pictures" "$HOME/pers/xdg/Pictures"
 mv "$HOME/Public" "$HOME/pers/xdg/Public"
 mv "$HOME/Templates" "$HOME/pers/xdg/Templates"
 mv "$HOME/Videos" "$HOME/pers/xdg/Videos"
-
 mv "$HOME/Downloads" "$HOME/downloads"
 
+xdg-user-dirs-update
