69fd8bc05c158576b8b593790dfd4e12aa17449a
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2025-10-05 19:22:08 +0300

    feat

--- dotfiles/.config/nvim/lua/plugins/dap.lua
+++ dotfiles/.config/nvim/lua/plugins/dap.lua
@@ -63,22 +63,22 @@ return {
 			vim.api.nvim_set_hl(0, "RedCursor", { fg = "#AF0000", bg = None })
 			vim.fn.sign_define("DapBreakpoint", { text = "⦿", texthl = "RedCursor", linehl = "", numhl = "" })
 
-			-- vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
-			-- vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
-			--
-			-- -- Eval var under cursor
-			-- vim.keymap.set("n", "<space>e", function()
-			-- 	require("dapui").eval(nil, { enter = true })
-			-- end)
-			--
-			-- vim.keymap.set("n", "<F1>", dap.continue)
-			-- vim.keymap.set("n", "<F2>", dap.step_into)
-			-- vim.keymap.set("n", "<F3>", dap.step_over)
-			-- vim.keymap.set("n", "<F4>", dap.step_out)
-			-- vim.keymap.set("n", "<F5>", dap.step_back)
-			-- vim.keymap.set("n", "<F12>", dap.restart)
-			--
-			-- vim.keymap.set("n", "<F11>", dap.disconnect)
+			vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+			vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+
+			-- Eval var under cursor
+			vim.keymap.set("n", "<space>e", function()
+				require("dapui").eval(nil, { enter = true })
+			end)
+
+			vim.keymap.set("n", "<F1>", dap.continue)
+			vim.keymap.set("n", "<F2>", dap.step_into)
+			vim.keymap.set("n", "<F3>", dap.step_over)
+			vim.keymap.set("n", "<F4>", dap.step_out)
+			vim.keymap.set("n", "<F5>", dap.step_back)
+			vim.keymap.set("n", "<F12>", dap.restart)
+
+			vim.keymap.set("n", "<F11>", dap.disconnect)
 
 			dap.listeners.before.attach.dapui_config = function()
 				ui.open()
