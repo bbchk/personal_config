f0ca568f95e32bc7f633ce99f73aaf5bf0f4be4b
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2025-11-01 06:56:51 +0000

    feat

--- setup/common/filesystem.sh
+++ setup/common/filesystem.sh
@@ -16,6 +16,13 @@ mv "$HOME/Videos" "$HOME/pers/xdg/Videos"
 mv "$HOME/Downloads" "$HOME/downloads"
 
 # Clone frequently worked on projects
-
-mkdir -p "$HOME/dev"
-git clone git@github.com:bbchk/jv-fr.git "$HOME/dev/jv-fr"
+mkdir -p "$HOME/dev/my" "$HOME/dev/ib"
+git clone git@github.com:bbchk/jv-fr.git     "$HOME/dev/my/jv-fr"
+git clone git@github.com:bbchk/avkfe.git     "$HOME/dev/my/avkfe"
+git clone git@github.com:bbchk/scrape.git    "$HOME/dev/my/scrape"
+git clone git@github.com:bbchk/lvfe.git      "$HOME/dev/my/lvfe"
+git clone git@github.com:bbchk/live.git      "$HOME/dev/my/live"
+git clone git@github.com:bbchk/lvbe.git      "$HOME/dev/my/lvbe"
+git clone git@github.com:bbchk/lvops.git     "$HOME/dev/my/lvops"
+git clone git@github.com:bbchk/train.git     "$HOME/dev/my/train"
+git clone git@github.com:bbchk/slugtrans.git "$HOME/dev/my/slugtrans"
