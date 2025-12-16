6bce1c8ebeae5a964304d74247dce50ce255f780
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2025-12-16 19:57:05 +0200

    feat:

--- setup/k8s/interactive.sh
+++ setup/k8s/interactive.sh
@@ -2,22 +2,16 @@
 
 # --- Change Password ---
 read -p "Enter username: " USERNAME
-read -sp "Enter new password for $USERNAME: " NEW_PASSWORD
+read -p "Enter new password for $USERNAME: " NEW_PASSWORD
 echo
-echo "$USERNAME:$NEW_PASSWORD" | chpasswd
+echo "$USERNAME:$NEW_PASSWORD" | /usr/sbin/chpasswd
 
 # --- Change Hostname ---
 read -p "Enter new hostname: " NEW_HOSTNAME
-echo "$NEW_HOSTNAME" > /etc/hostname
+hostnamectl set-hostname "$NEW_HOSTNAME"
 sed -i "s/127.0.1.1.*/127.0.1.1\t$NEW_HOSTNAME/" /etc/hosts
 
-# Update hostname immediately (works in Debian 13)
-if command -v hostnamectl &> /dev/null; then
-    hostnamectl set-hostname "$NEW_HOSTNAME"
-else
-    hostname "$NEW_HOSTNAME"
-fi
-
+# --- Set up Tailscale ---
 read -p "Enter Tailscale auth key: " TS_AUTH_KEY
 curl -fsSL https://tailscale.com/install.sh | sh
 tailscale up --authkey="$TS_AUTH_KEY" --accept-routes
