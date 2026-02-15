38162d83ec947de3e66673bb7bb64bf6d5e72851
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2026-02-15 07:26:27 +0200

    new

--- setup/fedora/config.sh
+++ setup/fedora/config.sh
@@ -15,7 +15,8 @@ for i in "${dotfiles_to_symlink[@]}"; do
   base_item_name=$(basename "$i")
   log "Linking $i -> $HOME/$base_item_name"
   mv "$HOME/$base_item_name" "$HOME/${base_item_name}.backup" 2>/dev/null
-  ln -sf "$i" "$HOME/$base_item_name"
+  ln -sfnT "$i" "$HOME/$base_item_name"
+
 done
 
 # ---- secrets ---------------------------
@@ -23,13 +24,13 @@ log "Decrypting secret files using ansible-vault..."
 find "$HOME/pers/secrets" -type f -exec ansible-vault decrypt --vault-password-file "$HOME/pers/password" -- {} \;
 
 mv "$HOME/.ssh" "$HOME/.ssh.backup" 2>/dev/null
-ln -sf "$HOME/pers/secrets/ssh" "$HOME/.ssh"
+ln -sfnT "$HOME/pers/secrets/ssh" "$HOME/.ssh"
 
 mkdir -p "$HOME/pers/dotfiles/.config/keepassxc"
-ln -sf "$HOME/pers/secrets/passwords/keepassxc.ini" "$HOME/pers/dotfiles/.config/keepassxc/keepassxc.ini"
+ln -sfnT "$HOME/pers/secrets/passwords/keepassxc.ini" "$HOME/pers/dotfiles/.config/keepassxc/keepassxc.ini"
 
 log "Symlinking custom /etc/hosts file..."
 sudo mv /etc/hosts /etc/hosts.backup
-sudo ln -sf "$HOME/pers/secrets/hosts" /etc/hosts
+sudo ln -sfnT "$HOME/pers/secrets/hosts" /etc/hosts
 
 log "======= config.sh execution finished successfully ======="
