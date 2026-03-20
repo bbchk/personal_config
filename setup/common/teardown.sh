463185e1bb460ff43fcee1b4f6f57998bdb684f9
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2026-03-20 09:13:43 +0200

    feat

--- setup/common/teardown.sh
+++ setup/common/teardown.sh
@@ -6,6 +6,7 @@ source "$HOME/pers/setup/common/utils.sh"
 
 log "Configuring repository remotes and initializing local Git hooks."
 git remote add origin-ssh git@github.com:bbchk/personal_config.git
+git remote set-url --push origin git@github.com:USERNAME/REPOSITORY.git
 git config core.hooksPath .githooks
 
 log "Updating the system shell to ZSH and initiating a session logout to apply changes."
