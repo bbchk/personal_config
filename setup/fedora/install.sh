54fccd3aefa69f57bea5ab14ec7fcb8ee82eb47b
Bohdan Buchok <bohdanbuchok@gmail.com>
2026-05-15 07:30:33 +0300

    feat: install steam

--- setup/fedora/install.sh
+++ setup/fedora/install.sh
@@ -26,6 +26,8 @@ PKGS=(
   traceroute nmap-ncat bind-utils openfortivpn net-tools curl nmap
   # Files/System
   fd-find ripgrep zip unzip lsof stow qbittorrent swappy ppp android-tools scrcpy fira-code-fonts fontawesome-fonts
+  # Misc
+  steam
 )
 
 log "Preparing to install ${#PKGS[@]} system packages via DNF..."
