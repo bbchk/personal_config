9d1e1f15a796b3220e3942ff5d80b508692f39db
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2026-06-17 09:14:32 +0300

    feat(setup): optimize

--- scripts/setup/distros/ubuntu.sh
+++ scripts/setup/distros/ubuntu.sh
@@ -55,8 +55,8 @@ flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flath
 flatpak update -y
 
 log "Install keepassxc..."
-flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install --user flathub org.keepassxc.KeePassXC
+flatpak remote-add --user -y --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install --user -y flathub org.keepassxc.KeePassXC
 
 log "Installing docker"
 curl -fsSL https://get.docker.com -o /tmp/get-docker.sh
