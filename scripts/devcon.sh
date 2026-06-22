fe885393593bd427035bb34b424726eb76ed361b
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2026-06-22 16:47:26 +0300

    feat: devcon.sh

--- scripts/devcon.sh
+++ scripts/devcon.sh
@@ -11,5 +11,5 @@ log "Fixing SSH key permissions..."
 find "/root/pers/" -path '*/.ssh/*' -type f ! -name '*.pub' -exec chmod 600 {} + 2>/dev/null || true
 
 log "Copying sudoers..."
-cp "/root/pers/dotfiles/.custom/sys" /etc/sudoers.d/sudoers
+cp "/root/pers/dotfiles/.custom/sys/sudoers" /etc/sudoers.d/sudoers
 chmod 440 /etc/sudoers.d/sudoers
