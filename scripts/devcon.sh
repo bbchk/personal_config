22c7eafddccb1a14da23ed204ab6bb87b1bbf036
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2026-06-22 16:34:18 +0300

    feat: devcon

--- scripts/devcon.sh
+++ scripts/devcon.sh
@@ -6,12 +6,10 @@ source /usr/local/share/devcontainer-helpers/utils.sh
 
 log "Decrypting secrets..."
 git -C "$HOME/pers" checkout -- .
-# Here it requires passphrase which I don't rpovide, so it just fails and goes on
 
 log "Fixing SSH key permissions..."
-find "$TARGET_PATH/" -path '*/.ssh/*' -type f ! -name '*.pub' -exec chmod 600 {} + 2>/dev/null || true
+find "/root/pers/" -path '*/.ssh/*' -type f ! -name '*.pub' -exec chmod 600 {} + 2>/dev/null || true
 
-log "Copying sudoers to /etc/sudoers.d/sudoers..."
-CUSTOM_DIR="$DOTFILES_FULL/.custom/sys"
-cp "$CUSTOM_DIR/sudoers" /etc/sudoers.d/sudoers
+log "Copying sudoers..."
+cp "/root/pers/dotfiles/.custom/sys" /etc/sudoers.d/sudoers
 chmod 440 /etc/sudoers.d/sudoers
