d3ab290d2c3c476fef30b15f31e265acd1b24273
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2026-06-23 17:08:12 +0300

    feat(devcon): ros2

--- devcon/ros2/ubuntu-00.00-50.00.sh
+++ devcon/ros2/ubuntu-00.00-50.00.sh
@@ -2,17 +2,25 @@
 
 source /usr/local/share/devcontainer-helpers/utils.sh
 
-apt-get update
+apt-get update && apt-get upgrade
 apt-get install -y \
   git \
-  build-essential \
   wget \
-  curl \
-  python3 \
-  python3-pip \
-  python-is-python3 \
-  ripgrep \
-  unzip \
-  fzf \
-  zsh \
-  iproute2
+  curl
+
+apt install locales
+locale-gen en_US en_US.UTF-8
+update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+apt install software-properties-common
+add-apt-repository universe
+
+apt install curl -y
+export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F'"' '{print $4}')
+curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo ${UBUNTU_CODENAME:-${VERSION_CODENAME}})_all.deb"
+dpkg -i /tmp/ros2-apt-source.deb
+
+apt install ros-lyrical-desktop
+apt install ros-lyrical-ros-base
+source /opt/ros/lyrical/setup.zsh
