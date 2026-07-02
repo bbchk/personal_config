21f6be372f0c252f6b1d88b0938cfeac4c44239c
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2026-07-02 13:00:00 +0300

    feat(devcon): fix ros2

--- devcon/ros2/ubuntu-00.00-50.00.sh
+++ devcon/ros2/ubuntu-00.00-50.00.sh
@@ -8,6 +8,7 @@ locale-gen en_US en_US.UTF-8
 update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 
+apt-get update
 apt-get install -y software-properties-common
 add-apt-repository universe -y
 
